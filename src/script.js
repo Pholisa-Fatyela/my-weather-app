f06a26f4a0dec1798a1acc454ce80f5fc486056e
Pholisa Fatyela <pholisafatyela@gmail.com>
2024-02-26 12:54:42 +0200

    improving structure and fixing bugs

--- src/script.js
+++ src/script.js
@@ -2,20 +2,28 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayTemperature);
 }
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-form-input");
+
+  searchCity(searchInputElement.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
+
+searchCity("Khayelitsha");
 
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -43,9 +51,6 @@ function formatDate(date) {
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}`;
 }
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
 
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
